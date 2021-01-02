f8a7c78d761b080762ec3a941e05d39579eeee0c
sidrastogi97 <siddharth.rastogi3@wipro.com>
2021-01-02 14:00:08 +0530

    Setup Project with controller

---
+++ src/java/com/spring/beans/HomePageBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.beans;
+
+/**
+ *
+ * @author hp
+ */
+public class HomePageBean {
+    private String values;
+
+    public String getValues() {
+        return values;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
+    }
+}
---
+++ src/java/com/spring/controller/HomePageController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.spring.beans.HomePageBean;
+/**
+ *
+ * @author hp
+ */
+
+@Controller
+@RequestMapping("/index.htm")
+public class HomePageController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(ModelMap model) {
+        System.out.println("on method");
+        model.put("printme", "Siddharth !!");
+        return "index";
+    }
+}
