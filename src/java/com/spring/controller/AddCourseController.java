3fcefbaef809608f2f6dd8ec09cfff94e8dfa937
sidrastogi97 <siddharth.rastogi3@wipro.com>
2021-01-03 18:58:37 +0530

    refactoring the code

--- src/java/com/spring/controller/AddCourseController.java
+++ src/java/com/spring/controller/AddCourseController.java
@@ -5,36 +5,39 @@
  */
 package com.spring.controller;
 
+import com.mysql.cj.log.Log;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.spring.beans.AddCourseBean;
+import com.spring.model.AddCourseModel;
 import com.spring.dao.CourseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class AddCourseController {
-    @Autowired  
+
+    @Autowired
     CourseDao course;
 
     @RequestMapping(value = "/showCourse", method = RequestMethod.GET)
     public String showcourse(Model model) {
-        model.addAttribute("course", new AddCourseBean());
+        model.addAttribute("course", new AddCourseModel());
         return "addNewCourse";
     }
 
-    @RequestMapping(value="/saveCourse",method = RequestMethod.POST)
-    public String save(@ModelAttribute AddCourseBean cr) {
+    @RequestMapping(value = "/saveCourse", method = RequestMethod.POST)
+    public String save(@ModelAttribute AddCourseModel cr) {
         course.save(cr);
-        return "redirect:viewList";
+        return "redirect:/index.htm";
     }
 
-    @RequestMapping("/viewList")
-    public String viewlist(Model model) {
-        List<AddCourseBean> list = course.getCourses();
+    @RequestMapping("/index")
+    public String viewlist(ModelMap model) {
+        List<AddCourseModel> list = course.getCourses();
         model.addAttribute("course_list", list);
         return "index";
     }
